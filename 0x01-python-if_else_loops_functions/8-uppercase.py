70b3918217d9f8cb816cf23f6d10d4fcacfafc8e
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-21 14:00:04 +0100

    change lowercase to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) in range(97, 123):
+            print("{}".format(chr(ord(i)-32)), end="")
+            continue
+        print(i, end="")
