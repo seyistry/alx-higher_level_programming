1da1dadabb2a2c4a0f9236df2699b0e87ed53613
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-21 14:30:16 +0100

    fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        print(number % 10, end="")
-    else:
-        print(number % -10, end="")
+    print(number % 10, end="")
     return number
