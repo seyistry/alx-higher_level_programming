106b0e27e7b2fae05feccf1ea3e4d765ad86c606
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-23 00:58:37 +0100

    count cmd argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) == 0:
+    if len(argv) < 2:
         print(".")
     else:
-        print(f"{len(argv)} arguments:")
+        print(f"{len(argv) - 1} arguments:")
         for i in range(1,len(argv)):
             print(f"{i:} {argv[i]}")
 
