7e6400d451533542dc07d8518696ad68d18983c6
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-23 01:01:53 +0100

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) < 2:
-        print(".")
+        print("0 arguments.")
     else:
         print(f"{len(argv) - 1} arguments:")
         for i in range(1,len(argv)):
