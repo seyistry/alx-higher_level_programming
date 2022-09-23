ac428829b120e02682603c7967d7652c37d0e46e
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-23 01:18:38 +0100

    fix plurals

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     if len(argv) < 2:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(len(argv) - 1))
+        s = 's' if len(argv) > 2 else ''
+        print("{0} argument{1}:".format(len(argv) - 1, s))
         for i in range(1, len(argv)):
             print("{0}: {1}".format(i, argv[i]))
