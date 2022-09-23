2b552de6dbbc652cb44af02dc65ce8c263ea452e
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-23 01:13:01 +0100

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         print("{} arguments:".format(len(argv) - 1))
-        for i in range(1,len(argv)):
+        for i in range(1, len(argv)):
             print("{0}: {1}".format(i, argv[i]))
-
