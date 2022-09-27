e3c9ad1dc21c12c889990ac7ff3d0bda7ac7b65b
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-27 21:04:19 +0100

    print matrix list

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(len(i)):
+            if j == len(i) - 1:
+                print("{:d}".format(i[j]))
+                continue
+            print("{:d}".format(i[j]), end=" ")
