f927fd8af3b1413a8a9b20a6792b450e7a3f3405
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-28 22:33:22 +0100

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not str or roman_string is None:
+    if type(roman_string) is not str or roman_string == None:
         return 0
     form = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     cal = 0
