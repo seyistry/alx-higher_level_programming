7e422f59c12be58e73e1bd92d1d79ba9611d6031
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-28 21:33:17 +0100

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    top, val= None, 0
+    top, val = None, 0
     for key, value in a_dictionary.items():
         if value > val:
             top, val = key, value
