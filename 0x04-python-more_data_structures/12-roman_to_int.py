8b9990276ba9f629cbad33ec1a3f5b828e512be0
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-28 22:26:28 +0100

    convert Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    form = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    cal = 0
+    prev = 1000
+    for i in roman_string:
+        cal += form[i]
+        if form[i] > prev:
+            cal = cal - (prev * 2)
+        prev = form[i]
+    return cal
