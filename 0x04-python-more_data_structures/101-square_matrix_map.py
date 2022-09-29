8231899f4a90c8718f8a64fe34ce7f8d0794ad97
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-09-29 14:46:12 +0100

    fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map((lambda x: [i ** 2 for i in x]), matrix))
+    return list(map((lambda x: [x[0] ** 2, x[1] ** 2, x[2] ** 2]), matrix))
