d7211966a5e5447a6bbc99a6abfc3935edc7b5d0
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-10 19:24:58 +0100

    testing

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as err:
-        print("Exception: {}".format(err))
+        stderr.write("Exception: {}".format(err))
         return False
     return True
