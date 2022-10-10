be1a5ad43b7a8f1148dba6bd70cd1cbf567a2145
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-10 18:52:14 +0100

    add 100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as err:
+        print("Exception: {}".format(err))
+        return False
+    else:
+        return True
