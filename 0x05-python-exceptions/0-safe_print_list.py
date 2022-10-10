d789aafab5ee49cffa6807303b2c8ce1449f863a
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-10 16:31:22 +0100

    another test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,5 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     finally:
-        if x > 0:
-            print("")
-            return count
+        print()
+        return count
