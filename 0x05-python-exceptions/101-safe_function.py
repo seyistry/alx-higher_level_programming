2085c5f49d6fe06d99a96f2bb950b513ac369a8e
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-10 20:20:56 +0100

    add 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as err:
+        print(f"Exception: {err}", file=stderr)
+        return None
+    return result
