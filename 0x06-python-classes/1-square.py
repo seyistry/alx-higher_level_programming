74e2ccd58ce7d6381fc0c1405860b82917da5c83
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-12 19:41:30 +0100

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" defines a class"""
+
+
+class Square:
+    """ class Square that defines a square
+    """
+    def __init__(self, size):
+        """ initialize the size of square
+        
+
+        Args:
+            size (any): size of square
+        """
+        self.__size = size
