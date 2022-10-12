a01fe156e2a84d79f698cebcfc464b23015dceb6
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-12 22:48:07 +0100

    test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,71 +1,89 @@
 #!/usr/bin/python3
-""" defines a class"""
+"""defining a square"""
 
 
 class Square:
-    """ class Square that defines a square
+    """ represents a square
+    Attributes:
+        ___size (int): size of a side of the square
+        ___position (tuple): pos of the square
     """
     def __init__(self, size=0, position=(0, 0)):
-        """ __init__ nethod to initialize the size of square
-        """
-        self.__size = size
-        self.__position = position
-
-    @property
-    def position(self):
-        """ setter size nethod to initialize the size of square
-        """
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        """ setter size nethod to initialize the size of square
+        """Initializes square
         Args:
-            value (tuple): size of square
-        Raises:
-            TypeError: if `size` is not an integer
+            size (int): size of a side of the square
+            position (tuple): pos of the square
+        Returns: None
         """
-        if (type(value) is not tuple or len(value) != 2 or
-                type(value[0]) is not int or value[0] < 0 or
-                type(value[1]) is not int or value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """calculates square's area
+        Returns: None
+        """
+        return (self.__size) ** 2
 
     @property
     def size(self):
-        """ setter size nethod to initialize the size of square
+        """getter of __size
+        Returns:
+            The square's size
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ setter size nethod to initialize the size of square
+        """setter of __size
         Args:
-            value (int): size of square
-        Raises:
-            TypeError: if `size` is not an integer
-            ValueError: If `size` is less than 0.
+            value (int): the square's size
+        Returns:
+            None
         """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """ A class method area that calculate
-        the area of a square
-        Returns:
-            int: return the square of size
-        """
-        return self.__size * self.__size
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
     def my_print(self):
-        """ print space and #
+        """Prints square
+        Returns:
+            None
         """
         if self.__size == 0:
             print()
             return
-        for i in range(self.__size):
-            print(f"{' ' * self.__position[0]}{'#' * self.__size}")
+        for i in range(self.__position[1]):
+            print()
+        for b in range(self.__size):
+            for c in range(self.__position[0]):
+                print(" ", end="")
+            for d in range(self.__size):
+                print("#", end="")
+            print("")
+
+    @property
+    def position(self):
+        """getter of __position
+        Returns:
+            position of the square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter of __position
+        Args:
+            value (tuple): position of the square
+        Returns:
+            None
+        """
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
