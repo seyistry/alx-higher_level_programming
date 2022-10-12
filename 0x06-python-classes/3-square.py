252cbae1def0e052e0fb7ddbf4767b17d8648ff5
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-12 20:19:48 +0100

    add 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" defines a class"""
+
+
+class Square:
+    """ class Square that defines a square
+    """
+    def __init__(self, size=0):
+        """ __init__ nethod to initialize the size of square
+
+        Args:
+            size (int): size of square
+
+        Raises:
+            TypeError: if `size` is not an integer
+            ValueError: If `size` is less than 0.
+
+        """
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """ A class method area that calculate 
+        the area of a square
+
+        """
+        return self.__size * self.__size
