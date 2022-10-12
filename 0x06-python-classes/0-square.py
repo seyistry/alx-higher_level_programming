2d7d679092af51cb61d58e69f580dd01ed2151f0
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-12 19:28:27 +0100

    update style

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 class Square:
-    """ A python Class
-    that defines a square
+    """The summary line for a class docstring should fit on one line.
+
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. If ``napoleon_attr_annotations``
+    is True, types can be specified in the class body using ``PEP 526``
+    annotations.
+
+    Attributes:
+        attr1: Description of `attr1`.
+        attr2: Description of `attr2`.
+
     """
     pass
