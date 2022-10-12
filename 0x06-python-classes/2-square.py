68857286c558fceea92f435bbb2f43db587947be
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-12 20:02:52 +0100

    fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,9 +8,13 @@ class Square:
     def __init__(self, size=0: int):
         """ initialize the size of square
 
-
         Args:
             size: size of square
+
+        Raises:
+            TypeError: if `size` is not an integer
+            ValueError: If `size` is less than 0.
+
         """
         self.__size = size
         if type(self.__size) is not int:
