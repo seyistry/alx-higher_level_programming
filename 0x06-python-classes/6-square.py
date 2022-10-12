7cbd30a483a3532fa8bbba7998b49a813a47b60f
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-12 22:40:40 +0100

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -67,7 +67,9 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for i in range(self.__position[1]):
-            print()
         for i in range(self.__size):
-            print(f"{' ' * self.__position[0]}{'#' * self.__size}")
+            for j in range(self.__position[0]):
+                print(" ", end="")
+            for k in range(self.__size):
+                print("#", end="")
+            print("")
