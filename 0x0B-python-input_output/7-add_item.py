b1d1443076d0b417b42224ad7fee2bea79e13793
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-26 01:24:40 +0100

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,13 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
+
+try:
+    old_list = load_from_json_file(filename)
+except:
+    old_list = []
+
 new_argv = argv[1:]
-old_list = load_from_json_file("add_item.json")
-save_to_json_file(old_list + new_argv, "add_item.json")
+
+save_to_json_file(old_list + new_argv, filename)
