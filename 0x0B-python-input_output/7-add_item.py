8cf7e77fa97d98ce4a9f72507ffc0e25d3b04a63
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-10-26 01:43:17 +0100

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,19 @@
 """
 
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
 try:
-    old_list = load_from_json_file(filename)
-except Exception:
-    old_list = []
+    j_list = load_from_json_file(filename)
 
-new_argv = argv[1:]
+except:
+    j_list = []
 
-save_to_json_file(old_list + new_argv, filename)
+for arguments in argv[1:]:
+    j_list.append(arguments)
+
+save_to_json_file(j_list, filename)
