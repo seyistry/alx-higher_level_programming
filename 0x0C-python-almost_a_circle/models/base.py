595809e430cf20512ca3a1de62c2a1fb76c20cd0
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-11-02 00:08:21 +0100

    return json rep

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """A Base class for this module"""
 
+import json
+
 
 class Base:
     """Base Class
@@ -19,3 +21,6 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        return json.dumps(list_dictionaries)
