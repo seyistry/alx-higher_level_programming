1773dfc45f9cd359b31152a13c8f9bf6f0c2dc75
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-11-01 18:18:49 +0100

    add base clase

---
+++ 0x0C-python-almost_a_circle0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A Base class for this module"""
+
+
+class Base:
+    """Base Class
+       with private attr __nb_object
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ base constructor
+
+        Args:
+            id (int, None): can be a None or a +int
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
