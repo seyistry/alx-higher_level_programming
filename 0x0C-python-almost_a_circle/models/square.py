50f86c91873f2c37f109fd6d2544294f12208228
Oluwaseyi Egunjobi <seyistry@gmail.com>
2022-11-01 23:18:49 +0100

    add module square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A Rectangle model class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A simple Square module"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """class Square constructor
+
+        Args:
+            size (_type_): _description_
+            x (int): _description_. Defaults to 0.
+            y (int): _description_. Defaults to 0.
+            id (_type_): _description_. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self) -> str:
+        return f"[Square] ({self.id}) {self.x}/{self.y}> - {self.width}"
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.__width * self.__height
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
