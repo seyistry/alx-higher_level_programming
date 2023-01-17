2a4fe9c99be3253eeada36ae68f735fe7cfe1eb4
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-01-17 22:58:24 +0100

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status
+""" Python script that takes in a URL, sends a request to the URL
+    and displays the value of
+    he X-Request-Id variable found in the header of the response
     """
 
 from urllib.request import Request, urlopen
