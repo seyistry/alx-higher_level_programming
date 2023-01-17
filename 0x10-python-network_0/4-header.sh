c835d80ea08f2166ca5224b34bfe664641b4f83f
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-01-17 12:01:31 +0100

    update 2.0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL as an argument, 
-# sends a GET request to the URL, and displays the body of the response
+# Write a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -sX GET -H "X-School-User-Id: 98" "$1"
