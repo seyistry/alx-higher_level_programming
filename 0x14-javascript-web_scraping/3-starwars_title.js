42c86f6625d5f7af84994d9ae72f28182e616b55
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-09 22:49:53 +0100

    add ex 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
+request(url, (error, response, body) => {
+  if (error) console.error('error:', error);
+  // console.log('statusCode:', response && response.statusCode);
+  const data = JSON.parse(body);
+  console.log(data.title)
+});
