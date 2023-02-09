07ae48bf6a9db5105c71e7c0e4e5a93534cd08e5
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-09 22:12:21 +0100

    add ex 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@ const request = require('request');
 const args = process.argv;
 const url = args[2];
 request(url, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+  }
   console.log('code:', response && response.statusCode);
 });
