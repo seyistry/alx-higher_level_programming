9f1400e4cb6f993624b543800beaa0bfb6a77def
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-09 21:40:29 +0100

    test

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const args = process.argv;
 const filePath = args[2];
 const readableStream = fs.createReadStream(filePath, 'utf8');
-readableStream.on('error', (error) => console.log(`error: ${error.message}`));
+// readableStream.on('error', (error) => console.log(`error: ${error.message}`));
 readableStream.on('data', (chunk) => console.log(chunk));
