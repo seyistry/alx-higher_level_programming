a74a7c69735df89bf2be08ed426fec0075d58270
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-10 18:30:35 +0100

    add ex 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,14 +7,11 @@ const id = 18;
 let count = 0;
 request(url, (error, response, body) => {
   if (error) console.error('error:', error);
-  // console.log('statusCode:', response && response.statusCode);
   const data = JSON.parse(body);
   for (const i in data.results) {
-    // console.log(data.results[i]);
     for (const j in data.results[i].characters) {
-      // console.log(data.results[i].characters[j]);
       if (
-        data.results[i].characters[j] ==
+        data.results[i].characters[j] ===
         `https://swapi-api.alx-tools.com/api/people/${id}/`
       ) {
         count = count + 1;
