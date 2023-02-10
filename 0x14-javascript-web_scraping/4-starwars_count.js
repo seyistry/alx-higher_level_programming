98d7d9b29ab973a80a7ec06713cff6413b2c391e
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-10 18:35:00 +0100

    add ex 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,6 @@
 const request = require('request');
 const args = process.argv;
 const url = args[2];
-const id = 18;
 let count = 0;
 request(url, (error, response, body) => {
   if (error) console.error('error:', error);
