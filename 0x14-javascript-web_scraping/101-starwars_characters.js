41255faf09295025098702241061f2b4cd7a47c0
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-11 01:03:24 +0100

    add ex 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fetch = require('node-fetch');
 const args = process.argv;
+const obj = {};
 const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
 request(url, (error, response, body) => {
   if (error) console.error('error:', error);
@@ -10,16 +10,19 @@ request(url, (error, response, body) => {
   getName(data.characters);
 });
 
-const getName = async (data) => {
-  for (const url of data) {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data.name);
+const getName = async (content) => {
+  for (const i in content) {
+    request(content[i], (error, response, body) => {
+      if (error) console.error('error:', error);
+      const data = JSON.parse(body);
+      obj[i] = data.name;
+    });
   }
-
-  // request(url, (error, response, body) => {
-  //   if (error) console.error('error:', error);
-  //   const data = JSON.parse(body);
-  //   console.log(data.name);
-  // });
 };
+
+// cheat async await
+setTimeout(() => {
+  for (const i in obj) {
+    console.log(obj[i]);
+  }
+}, 5000);
